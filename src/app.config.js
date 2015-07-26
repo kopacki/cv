f969001a757b8db652fd2a2a2c581e95d1589763
Tomasz Kopacki <tomash.pl@gmail.com>
2015-07-26 17:56:19 +0200

    add missing resources

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,6 @@
 const locals = {
-  'pageTitle': 'Curriculum vitae'
+  'pageTitle': 'Curriculum vitae',
+  'footerNote': 'I hereby give consent for my personal data included in my offer to be processed for the purposes of recruitment, in accordance with the Personal Data Protection Act dated 29.08.1997 (uniform text: Journal of Laws of the Republic of Poland 2002 No 101, item 926 with further amendments).'
 };
 
 function appConfigFn($urlRouterProvider, $locationProvider) {
--- src/components/cv/directives/cvPageDirective.js
+++ src/components/cv/directives/cvPageDirective.js
@@ -4,6 +4,12 @@ function cvPageDirective () {
       template: require('./cvPage.html'),
       scope: {
         sections: '='
+      },
+      link: (scope) => {
+        let locals = scope.$root.locals;
+
+        scope.title = locals.pageTitle;
+        scope.note = locals.footerNote;
       }
   };
 }
